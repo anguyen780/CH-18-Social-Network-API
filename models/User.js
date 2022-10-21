374c8bf3c421359ea309389ed639b7d24214659a
Lux <anguyen780@gmail.com>
2022-10-21 17:49:10 -0500

    created a User model

---
+++ models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+// Schema to create User model
+const userSchema = new mongoose.Schema({
+    username: { type: String, unique: true, required: true, trim: true },
+    email: { type: String, required: true, unique: true, match: /.+\@.+\..+/ },
+    thoughts: [thoughtSchema],
+    friends: [userSchema]
+},
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+// Create a virtual property 'friendCount' that retrieves length of user's friends array field on query
+userSchema.virtual('friendCount').get(function(){
+    return this.friends.length;
+});
+
+// Initialize User model
+const User = model('User', userSchema);
+
+module.exports = User
