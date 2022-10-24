dbeb94cacce558d64110db984e536211d9ba0c79
Lux <anguyen780@gmail.com>
2022-10-24 17:53:01 -0500

    added functionality to deleteUser to delete thoughts associated with the user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,4 +1,4 @@
-const { User } = require('../models');
+const { User, Thought } = require('../models');
 
 module.exports = {
     // get all users
@@ -28,14 +28,19 @@ module.exports = {
             { _id: req.params.userId },
             { $set: req.body },
             { runValidators: true, new: true })
-        .then((user) => !user ? res.status(404).json({ message: 'No user with this ID'}) : res.json(user))
-        .catch((err) => res.status(500).json(err));
+            .then((user) => !user ? res.status(404).json({ message: 'No user with this ID' }) : res.json(user))
+            .catch((err) => res.status(500).json(err));
     },
-    // delete user
-    deleteUser(req, res){
+    // delete user and thoughts associated
+    deleteUser(req, res) {
         User.findOneAndDelete({ _id: req.params.userId })
-        .then((user) => !user ? res.status(404).json({ message: 'No user with this ID'}) : res.json(user))
-        .catch((err) => res.status(500).json(err));
+            .then((user) => !user ? res.status(404).json({ message: 'No user with this ID' }) : Thought.deleteMany(
+                { username: user.username },
+            )
+            )
+            .then((results) => !results ? res.status(404).json({ message: 'User deleted but could not delete thought' }) : res.json({ message: 'User and thoughts associated were deleted' })
+            )
+            .catch((err) => res.status(500).json(err));
     },
 
 };
